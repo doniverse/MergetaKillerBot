3da992f6345f5ad5a6c0ca33af2de61949f87fe3
Naol Fekadu Hunduma <nf.naol9@gmail.com>
2024-02-02 19:05:16 +0300

    initial and final commits

---
+++ index.js
@@ -0,0 +1,37 @@
+const {Bot, MemorySessionStorage }= require('grammy');
+const {chatMembers} = require('@grammyjs/chat-members');
+require('dotenv').config();
+// Create an instance of the `Bot` class and pass your bot token to it.
+console.log(process.env.BOT_TOKEN)
+const bot = new Bot(process.env.BOT_TOKEN); // <-- put your bot token between the ""
+
+// You can now register listeners on your bot object `bot`.
+const adapter = new MemorySessionStorage();
+bot.use(chatMembers(adapter));
+
+
+bot.catch('error', ctx =>{
+    console.log("Error ")
+})
+
+// Handle the /start command.
+bot.command("start", (ctx) =>{
+     ctx.reply("Just add me to your group, I will take care of the rest!")
+     allowed_updates: ["chat_member", "message"]
+    });
+
+// messages
+bot.on('message', ctx =>{
+    const full_name = `${ctx.message.from.first_name} ${ctx.message.from.last_name}`;
+    const user_id = ctx.message.from.id;
+    if(full_name.startsWith("መርጌታ") || full_name.endsWith("መርጌታ")){
+        ctx.banChatMember(user_id);
+        ctx.reply("Another መርጌታ eliminated!");
+    }
+
+})
+
+
+// Start the bot.
+bot.start();
+
