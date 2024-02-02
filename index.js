01ce00c1cdbcdb6702e7d1f8a032e99055aebcc5
Naol Fekadu Hunduma <nf.naol9@gmail.com>
2024-02-03 02:15:07 +0300

    meri update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {chatMembers} = require('@grammyjs/chat-members');
 require('dotenv').config();
 
 // Create an instance of the `Bot` class and pass your bot token to it.
-const bot = new Bot(process.env.BOT_TOKEN); // <-- put your bot token between the ""
+const bot = new Bot("6745608618:AAEWljbsw1jdmepScT8HXXP6zKN9OR5xSAo"); // <-- put your bot token between the ""
 
 // You can now register listeners on your bot object `bot`.
 const adapter = new MemorySessionStorage();
@@ -24,7 +24,9 @@ bot.command("start", (ctx) =>{
 bot.on('message', ctx =>{
     const full_name = `${ctx.message.from.first_name} ${ctx.message.from.last_name}`;
     const user_id = ctx.message.from.id;
-    if(full_name.startsWith("መርጌታ") || full_name.endsWith("መርጌታ") || full_name.includes("መርጌታ")){
+    const message = ctx.message.text;
+    console.log(full_name,ctx)
+    if(full_name.includes("መርጌታ") || full_name.includes("መሪጌታ") || (message.includes("መሪጌታ") && (message.includes("09") || message.includes("+251"))) || (message.includes("መርጌታ") && (message.includes("09") || message.includes("+251")))){
         try {
             ctx.banChatMember(user_id);
             ctx.reply("Another መርጌታ eliminated!");
